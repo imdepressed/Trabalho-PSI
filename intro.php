9ca79983f3d043efd4a10c6bb594c2d2cc0b0c22
Pedro Rodrigues <35767085+RaccoonPT@users.noreply.github.com>
2018-02-14 18:19:53 +0000

    Add files via upload

---
+++ intro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>P&aacutegina Inicial</title>
+	<link rel="stylesheet" type="text/css"  href="style.css">
+
+</head>
+<body style="background:url(https://advocatesmd.com/wp-content/uploads/2017/12/light-blue-background-wallpaper-1.jpg);font-family: sans-serif;margin: 0">
+	<ul style="margin-top: 0px">
+	  <li><a class="active" href="intro.php">Home</a></li>
+	  <li><a href="login.php">Login</a></li>
+	  <li><a href="registo.html">Registo</a></li>
+	</ul>
+	<div style="position: absolute;top: 50%;left: 50%;transform: translateX(-50%) translateY(-50%);">
+		<h1 style="font-size: 60px;color: #3f9dd6" align="center">Bem Vindo</h1>
+		<div align="center">
+			<img src="https://www.ocrmobile.com/images/2016/12/21/cloud.png"><br>
+			<a href="login.php" style="margin-right: 100px;color: #154c6d;font-weight: bold;">Login</a>
+			<a style="color: #154c6d;font-weight: bold;" href="registo.html">Registe-se</a>
+		</div>
+	</div>
+</body>
+</html>
