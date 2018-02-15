3326a8523e3759293e87c1e2162ec87f34a20764
Ricardo Cenoura <35767189+imdepressed@users.noreply.github.com>
2018-02-15 13:34:37 +0000

    Update intro.php

--- intro.php
+++ intro.php
@@ -19,6 +19,6 @@
 			<a style="color: #154c6d;font-weight: bold;" href="registo.html">Registe-se</a>
 		</div>
 	</div>
-	<!-- Mensagem de Alteração o.O -->
+	<!-- Tive aqui Cenoura -->
 </body>
 </html>
